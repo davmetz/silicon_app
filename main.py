facebeae423fc0141f76814194b121fdbbb14bdb
David Metz <david.metz67@gmail.com>
2022-10-11 22:56:48 +0200

    add script to convert automatically ui to py

--- main.py
+++ main.py
@@ -10,16 +10,11 @@ def update_in_dev():
         # pyuic5 -x /designer/mainwindow.ui -o /silicon_app/ui/mainwindow_ui.py
         import pathlib
         path= pathlib.Path(__file__).parent.resolve()
-        print(f'{path=}')
-        p_src= path / 'designer/mainwindow.ui'
-        p_out= path / 'silicon_app/ui/mainwindow_ui.py'
-        print(f'{p_src=}')
-        bashCommand = "E:"
-        process = subprocess.Popen(bashCommand.split(), cwd=path)
-        bashCommand = f"pyuic5 -x {p_src} -o {p_out}"
-        process = subprocess.Popen(bashCommand.split(), cwd=path)
+        # process = subprocess.Popen(bashCommand)
+        bashCommand = f".\script.bat"
+        process = subprocess.Popen(bashCommand.split())
 
 
 if __name__ == '__main__':
-    # update_in_dev()
+    update_in_dev()
     sys.exit(app.run())
---
+++ script.bat
@@ -0,0 +1,4 @@
+call C:/Anaconda3/Scripts/activate.bat E:\Software_dev\Python\silicon_app\ C:/Anaconda3
+call conda activate ai-app
+call E:
+call pyuic5 -x E:/Software_dev/Python/silicon_app/designer/mainwindow.ui -o E:/Software_dev/Python/silicon_app/silicon_app/ui/mainwindow_ui.py
